7c1eefdf2b1c3959164cf380a97128f89dd7cd13
prate <lucas.prates@mail.utoronto.ca>
2025-11-19 21:33:58 -0500

    updated animation script to make nicer figures

--- animate.py
+++ animate.py
@@ -14,37 +14,44 @@ M=np.ceil(u.max().data)
 x, y = ds.x, ds.y
 nframes = len(ds.time.values)
 print(nframes)
-fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, figsize=(8, 18))
+# fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, figsize=(8, 18))
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(8, 18), constrained_layout=True)
 
 c1 = ax1.pcolormesh(x, y, zeta.isel(time=0), cmap="coolwarm", vmin=-M*10, vmax=M*10)
 c2 = ax2.pcolormesh(x, y, u.isel(time=0), cmap="coolwarm", vmin=-M, vmax=M)
 c3 = ax3.pcolormesh(x, y, v.isel(time=0), cmap="coolwarm", vmin=-M, vmax=M)
-c4 = ax4.pcolormesh(x, y, psi.isel(time=0), cmap="coolwarm", vmin=-M/10, vmax=M/10)
+# c4 = ax4.pcolormesh(x, y, psi.isel(time=0), cmap="coolwarm", vmin=-M/10, vmax=M/10)
 
 ax1.set_title('Vorticity')
-ax2.set_title('u')
-ax3.set_title('v')
-ax4.set_title('Streamfunction')
+ax2.set_title('Zonal Velocity')
+ax3.set_title('Meridional Velocity')
+# ax4.set_title('Streamfunction')
+
+fig.colorbar(c1, ax=ax1)
+fig.colorbar(c2, ax=ax2)
+fig.colorbar(c3, ax=ax3)
+# fig.colorbar(c4, ax=ax4)
 plt.show()
 
 
 def __update(frame):
-    global c1, c2, c3, c4
+    global c1, c2, c3#, c4
 
     # Remove the data
     c1.remove()
     c2.remove()
     c3.remove()
-    c4.remove()
+    # c4.remove()
     # Draw the new frame
     c1 = ax1.pcolormesh(x, y, zeta.isel(time=frame), cmap="coolwarm", vmin=-M*10, vmax=M*10)
     c2 = ax2.pcolormesh(x, y, u.isel(time=frame), cmap="coolwarm", vmin=-M, vmax=M)
     c3 = ax3.pcolormesh(x, y, v.isel(time=frame), cmap="coolwarm", vmin=-M, vmax=M)
-    c4 = ax4.pcolormesh(x, y, psi.isel(time=frame), cmap="coolwarm", vmin=-M/10, vmax=M/10)
+    # c4 = ax4.pcolormesh(x, y, psi.isel(time=frame), cmap="coolwarm", vmin=-M/10, vmax=M/10)
 
-    return [c1, c2, c3, c4]
+    # return [c1, c2, c3, c4]
+    return [c1, c2, c3, c3]
 
-ani = FuncAnimation(fig=fig, func=__update, frames=range(len(ds.time)), interval=100)#interval=100 is real time
+ani = FuncAnimation(fig=fig, func=__update, frames=range(len(ds.time)), interval=100)
 
 ani.save('animation.mp4')
 plt.show()
