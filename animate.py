0093c9681af499b3f15a8ddc7885ae1423447e69
prate <lucas.prates@mail.utoronto.ca>
2025-12-16 16:38:25 -0500

    Added time counter to animations

--- animate.py
+++ animate.py
@@ -2,36 +2,44 @@ import xarray as xr
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import seaborn as sns
 
 ds = xr.open_dataset('nc_files/zonal_jet_output.nc')
 u = ds.u
 v = ds.v
 zeta = ds.vorticity
-step=10
+t = ds.time
+
 MZ=np.ceil(zeta.max().data)
 M=np.ceil(u.max().data)
 x, y = ds.x, ds.y
 nframes = len(ds.time.values)
-print(nframes)
-fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(8, 12), constrained_layout=True)
 
+sns.set_context('talk')
+fig, axes = plt.subplots(3, 1, figsize=(6, 12), constrained_layout=True)
+
+for ax in axes:
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+fig.suptitle(r'$t=0.00$')
+
+ax1, ax2, ax3 = axes
 c1 = ax1.pcolormesh(x, y, zeta.isel(time=0), cmap="coolwarm", vmin=-MZ, vmax=MZ)
 c2 = ax2.pcolormesh(x, y, u.isel(time=0), cmap="coolwarm", vmin=-M, vmax=M)
 c3 = ax3.pcolormesh(x, y, v.isel(time=0), cmap="coolwarm", vmin=-M, vmax=M)
 
 ax1.set_title('Vorticity')
-ax2.set_title('Zonal Velocity')
-ax3.set_title('Meridional Velocity')
+ax2.set_title('Horiz. Velocity')
+ax3.set_title('Vert. Velocity')
 
-# square axes 
-# ax1.set_aspect('equal')
-# ax2.set_aspect('equal')
-# ax3.set_aspect('equal')
 
 # set colorbars
 fig.colorbar(c1, ax=ax1)
 fig.colorbar(c2, ax=ax2)
 fig.colorbar(c3, ax=ax3)
+
+plt.savefig('figures/initial_truth.png')
 
 def __update(frame):
     global c1, c2, c3, nframes
@@ -42,6 +50,8 @@ def __update(frame):
     c2.remove()
     c3.remove()
     
+    tf = round(t.data[frame], 2)
+    fig.suptitle(r'$t=$'+f'{tf:.2f}')
     # Draw the new frame
     c1 = ax1.pcolormesh(x, y, zeta.isel(time=frame), cmap="coolwarm", vmin=-MZ, vmax=MZ)
     c2 = ax2.pcolormesh(x, y, u.isel(time=frame), cmap="coolwarm", vmin=-M, vmax=M)
--- animate_ens.py
+++ animate_ens.py
@@ -2,11 +2,13 @@ import xarray as xr
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import matplotlib.ticker as ticker
+import seaborn as sns
 
 nens=100
 bscale = 1
-rscale = 5#1000000
-tobs = -1#1000#500
+rscale = 5.0#1000000
+tobs = 1000#1000#500
 o = 3
 
 if tobs==-1:
@@ -20,10 +22,14 @@ ds = xr.open_dataset(f'nc_files/' + inname)
 u = ds.u
 v = ds.v
 zeta = ds.vorticity
+t = ds.time
 
+# get variables
 ua = ds.u_ens.mean('ensemble_id')
 va = ds.v_ens.mean('ensemble_id')
 zetaa = ds.vorticity_ens.mean('ensemble_id')
+
+# compute variance and corr
 zeta_var = ds.vorticity_var
 zeta_corr = ds.vorticity_corr
 
@@ -46,28 +52,36 @@ V=zeta_var.max().data
 x, y = ds.x, ds.y
 nframes = len(ds.time.values)
 
-fig, axes2d = plt.subplots(4, 2, figsize=(12, 12), constrained_layout=True)
+sns.set_context('talk')
+fig, axes2d = plt.subplots(4, 2, figsize=(8, 12), constrained_layout=True)
 
-ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8 = axes2d.ravel()
+axes = axes2d.ravel()
+for ax in axes:
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+fig.suptitle(r'$t=0.00$')
+
+ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8 = axes
 c1 = ax1.pcolormesh(x, y, zeta.isel(time=0), cmap="coolwarm", vmin=-MZ, vmax=MZ)
 c3 = ax3.pcolormesh(x, y, u.isel(time=0), cmap="coolwarm", vmin=-M, vmax=M)
 c5 = ax5.pcolormesh(x, y, v.isel(time=0), cmap="coolwarm", vmin=-M, vmax=M)
 c2 = ax2.pcolormesh(x, y, zetaa.isel(time=0), cmap="coolwarm", vmin=-MZ, vmax=MZ)
 c4 = ax4.pcolormesh(x, y, ua.isel(time=0), cmap="coolwarm", vmin=-M, vmax=M)
 c6 = ax6.pcolormesh(x, y, va.isel(time=0), cmap="coolwarm", vmin=-M, vmax=M)
-c8 = ax7.pcolormesh(x, y, obsmask, cmap="viridis", vmin=0, vmax=1)
-# c8 = ax8.pcolormesh(x, y, zeta_var.isel(time=0), cmap="viridis", vmin=0, vmax=V)
-c8 = ax8.pcolormesh(x, y, zeta_corr.isel(time=0), cmap="PRGn", vmin=-1, vmax=1)
+c7 = ax7.pcolormesh(x, y, obsmask, cmap="viridis", vmin=0, vmax=1)
+c8 = ax8.pcolormesh(x, y, zeta_var.isel(time=0), cmap="viridis", vmin=0, vmax=V)
+# c8 = ax8.pcolormesh(x, y, zeta_corr.isel(time=0), cmap="PRGn", vmin=-1, vmax=1)
 
-ax1.set_title('Vorticity')
-ax3.set_title('Zonal Velocity')
-ax5.set_title('Meridional Velocity')
-ax2.set_title('Vorticity')
-ax4.set_title('Zonal Velocity')
-ax6.set_title('Meridional Velocity')
+ax1.set_title('Vorticity (Truth)')
+ax3.set_title('Horiz. Velocity (Truth)')
+ax5.set_title('Vert. Velocity (Truth)')
+ax2.set_title('Vorticity (Forecast)')
+ax4.set_title('Horiz. Velocity (Forecast)')
+ax6.set_title('Vert. Velocity (Forecast)')
 ax7.set_title('Observing Network')
-# ax8.set_title(f'Variance/MSE (RMSE={round(rmse[0], 2)})')
-ax8.set_title(f'Correlation')
+ax8.set_title(f'Forecast Variance/MSE')
+# ax8.set_title(f'Correlation')
 
 fig.colorbar(c1, ax=ax1)
 fig.colorbar(c2, ax=ax2)
@@ -75,12 +89,15 @@ fig.colorbar(c3, ax=ax3)
 fig.colorbar(c4, ax=ax4)
 fig.colorbar(c5, ax=ax5)
 fig.colorbar(c6, ax=ax6)
-fig.colorbar(c8, ax=ax8)
-# plt.show()
+fig.colorbar(c7, ax=ax7)
+fmt = ticker.ScalarFormatter()
+fmt.set_powerlimits((0, 0)) 
+fig.colorbar(c8, ax=ax8, format=fmt)
+plt.show()
 
 
 def __update(frame):
-    global c1, c2, c3, c4, c5, c6, c8,  nframes
+    global c1, c2, c3, c4, c5, c6, c8, nframes
 
     print(f'frame {frame}/{nframes}')
     # Remove the data
@@ -92,6 +109,8 @@ def __update(frame):
     c6.remove()
     c8.remove()
     
+    tf = round(t.data[frame], 2)
+    fig.suptitle(r'$t=$'+f'{tf:.2f}')
     # Draw the new frame
     c1 = ax1.pcolormesh(x, y, zeta.isel(time=frame), cmap="coolwarm", vmin=-MZ, vmax=MZ)
     c3 = ax3.pcolormesh(x, y, u.isel(time=frame), cmap="coolwarm", vmin=-M, vmax=M)
@@ -99,11 +118,11 @@ def __update(frame):
     c2 = ax2.pcolormesh(x, y, zetaa.isel(time=frame), cmap="coolwarm", vmin=-MZ, vmax=MZ)
     c4 = ax4.pcolormesh(x, y, ua.isel(time=frame), cmap="coolwarm", vmin=-M, vmax=M)
     c6 = ax6.pcolormesh(x, y, va.isel(time=frame), cmap="coolwarm", vmin=-M, vmax=M)
-    # c8 = ax8.pcolormesh(x, y, zeta_var.isel(time=frame), cmap="viridis", vmin=0, vmax=V)
-    c8 = ax8.pcolormesh(x, y, zeta_corr.isel(time=frame), cmap="PRGn", vmin=-1, vmax=1)
+    c8 = ax8.pcolormesh(x, y, zeta_var.isel(time=frame), cmap="viridis", vmin=0, vmax=V)
+    # c8 = ax8.pcolormesh(x, y, zeta_corr.isel(time=frame), cmap="PRGn", vmin=-1, vmax=1)
     # ax8.set_title(f'Variance/MSE (RMSE={round(rmse[frame], 2)})')
 
-    return [c1, c2, c3, c4, c5, c6, c8]
+    return [c1, c2, c3, c4, c5, c6, c7, c8]
 
 ani = FuncAnimation(fig=fig, func=__update, frames=range(len(ds.time)), interval=100)
 
