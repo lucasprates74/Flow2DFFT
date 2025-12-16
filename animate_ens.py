4c0d7308dd875682c93375d72e74e0a56e34d221
prate <lucas.prates@mail.utoronto.ca>
2025-12-16 16:38:47 -0500

    Removed bottom row from ensemble animations

--- animate_ens.py
+++ animate_ens.py
@@ -69,8 +69,8 @@ c5 = ax5.pcolormesh(x, y, v.isel(time=0), cmap="coolwarm", vmin=-M, vmax=M)
 c2 = ax2.pcolormesh(x, y, zetaa.isel(time=0), cmap="coolwarm", vmin=-MZ, vmax=MZ)
 c4 = ax4.pcolormesh(x, y, ua.isel(time=0), cmap="coolwarm", vmin=-M, vmax=M)
 c6 = ax6.pcolormesh(x, y, va.isel(time=0), cmap="coolwarm", vmin=-M, vmax=M)
-c7 = ax7.pcolormesh(x, y, obsmask, cmap="viridis", vmin=0, vmax=1)
-c8 = ax8.pcolormesh(x, y, zeta_var.isel(time=0), cmap="viridis", vmin=0, vmax=V)
+# c7 = ax7.pcolormesh(x, y, obsmask, cmap="viridis", vmin=0, vmax=1)
+# c8 = ax8.pcolormesh(x, y, zeta_var.isel(time=0), cmap="viridis", vmin=0, vmax=V)
 # c8 = ax8.pcolormesh(x, y, zeta_corr.isel(time=0), cmap="PRGn", vmin=-1, vmax=1)
 
 ax1.set_title('Vorticity (Truth)')
@@ -79,8 +79,8 @@ ax5.set_title('Vert. Velocity (Truth)')
 ax2.set_title('Vorticity (Forecast)')
 ax4.set_title('Horiz. Velocity (Forecast)')
 ax6.set_title('Vert. Velocity (Forecast)')
-ax7.set_title('Observing Network')
-ax8.set_title(f'Forecast Variance/MSE')
+# ax7.set_title('Observing Network')
+# ax8.set_title(f'Forecast Variance/MSE')
 # ax8.set_title(f'Correlation')
 
 fig.colorbar(c1, ax=ax1)
@@ -89,15 +89,15 @@ fig.colorbar(c3, ax=ax3)
 fig.colorbar(c4, ax=ax4)
 fig.colorbar(c5, ax=ax5)
 fig.colorbar(c6, ax=ax6)
-fig.colorbar(c7, ax=ax7)
-fmt = ticker.ScalarFormatter()
-fmt.set_powerlimits((0, 0)) 
-fig.colorbar(c8, ax=ax8, format=fmt)
+# fig.colorbar(c7, ax=ax7)
+# fmt = ticker.ScalarFormatter()
+# fmt.set_powerlimits((0, 0)) 
+# fig.colorbar(c8, ax=ax8, format=fmt)
 plt.show()
 
 
 def __update(frame):
-    global c1, c2, c3, c4, c5, c6, c8, nframes
+    global c1, c2, c3, c4, c5, c6, nframes
 
     print(f'frame {frame}/{nframes}')
     # Remove the data
@@ -107,7 +107,6 @@ def __update(frame):
     c4.remove()
     c5.remove()
     c6.remove()
-    c8.remove()
     
     tf = round(t.data[frame], 2)
     fig.suptitle(r'$t=$'+f'{tf:.2f}')
@@ -118,11 +117,11 @@ def __update(frame):
     c2 = ax2.pcolormesh(x, y, zetaa.isel(time=frame), cmap="coolwarm", vmin=-MZ, vmax=MZ)
     c4 = ax4.pcolormesh(x, y, ua.isel(time=frame), cmap="coolwarm", vmin=-M, vmax=M)
     c6 = ax6.pcolormesh(x, y, va.isel(time=frame), cmap="coolwarm", vmin=-M, vmax=M)
-    c8 = ax8.pcolormesh(x, y, zeta_var.isel(time=frame), cmap="viridis", vmin=0, vmax=V)
+    # c8 = ax8.pcolormesh(x, y, zeta_var.isel(time=frame), cmap="viridis", vmin=0, vmax=V)
     # c8 = ax8.pcolormesh(x, y, zeta_corr.isel(time=frame), cmap="PRGn", vmin=-1, vmax=1)
     # ax8.set_title(f'Variance/MSE (RMSE={round(rmse[frame], 2)})')
 
-    return [c1, c2, c3, c4, c5, c6, c7, c8]
+    return [c1, c2, c3, c4, c5, c6]
 
 ani = FuncAnimation(fig=fig, func=__update, frames=range(len(ds.time)), interval=100)
 
